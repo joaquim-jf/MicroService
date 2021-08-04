7dca0cafca0f9bd8de0ce47dbd2ffc9b346c3e18
joaquim <65045562+joaquim-jf@users.noreply.github.com>
2021-08-04 13:46:45 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# MicroService
+ projeto em Java com Spring Cloud, feito no curso da digitalinnovation.one
---
+++ shopping-cart/src/main/java/digitalinnovation/one/experts/shoppingcart/ShoppingCartApplication.java
@@ -0,0 +1,15 @@
+package digitalinnovation.one.experts.shoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.*;
+
+@SpringBootApplication
+public class ShoppingCartApplication {
+
+	public static void main(String[] args) {
+		run(ShoppingCartApplication.class, args);
+	}
+
+}
